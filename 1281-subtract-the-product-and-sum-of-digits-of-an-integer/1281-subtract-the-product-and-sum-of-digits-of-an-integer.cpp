178127c40ff6a7e282c4025020b1ef98b91de42e
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-09-25 11:22:44 +0530

    Time: 6 ms (7.01%), Space: 6 MB (26.08%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        int x=n;
-        int res=1;
         
-        while(x!=0){
-            res=(res)*(x%10);
-            x=x/10;
-        }
-        int sum=0;
+        int res=1;
+         int sum=0;
+        
+       
+       
         while(n!=0){
             sum=sum+n%10;
+            res=(res)*(n%10);
             n=n/10;
         }
         return (res-sum);
