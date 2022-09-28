42303bf652bf8c13f9484f9d915ab3e81440b823
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-09-28 22:17:16 +0530

    Time: 861 ms (5.01%), Space: 84.5 MB (45.29%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isfeasible(int n,vector<int>& a,int ans){
-        // if(ans=0)
-        //     return false;
+       
         for(int i=0;i<a.size();i++){
             n-=(a[i]/ans);
             if(a[i]%ans!=0)
@@ -14,14 +13,13 @@ public:
         
     }
     int minimizedMaximum(int n, vector<int>& q) {
-     long long int i=1,j=(int)1e5;
+     long long int i=1,j=0;
         int res=-1;
         
         for(int a=0;a<q.size();a++){
             j+=q[a];
         }
-       // if(n==1 || q.size()==1)
-       //      return j;
+     
         while(i<=j){
           long long int mid=(i+j)/2;
             if(isfeasible(n,q,mid)){
