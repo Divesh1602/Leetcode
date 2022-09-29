c861b1a5c63a2596366591b0f9fb40a2c2822eb3
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-09-29 11:11:51 +0530

    Time: 500 ms (24.05%), Space: 194.7 MB (15.48%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isfeasible(vector<int> A,long long k,int ans){
+        int sum=0;
+        for(auto a:A){
+            k-=a/ans;
+            // sum+=a%ans;
+            // k-=sum/ans;
+            
+            }
+         if(k<=0)
+                return true;
+         return false;
+        }
+       
+    
+    int maximumCandies(vector<int>& candies, long long k) {
+        int i=1;
+       long long int j=0;
+        int res=0;
+        for(auto a:candies){
+            j+=a;
+        }
+        while(i<=j){
+           long long int mid=(i+j)/2;
+            if(isfeasible(candies,k,mid)){
+                res=mid;
+                i=mid+1;
+            }
+            else
+                j=mid-1;
+        }
+        return res;
+    }
+};
