df59527035233fff8db84801a7d4dd1afb30cbc8
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-10-08 23:00:41 +0530

    Time: 171 ms (49.52%), Space: 19.9 MB (63.39%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -2,26 +2,48 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& a) {
        vector<vector<int>> res;
-        set<vector<int>> s;
         int n=a.size();
         sort(a.begin(),a.end());
-        for(int i=0;i<n-2;i++){
+        for(int i=0;i<n-2;){
             int j=i+1,k=n-1;
-            while(j<k){
+            
+            while(j<k){ 
                 int sum=a[i]+a[j]+a[k];
                 if(sum==0){
-                    s.insert({a[i],a[j],a[k]});
+                    res.push_back({a[i],a[j],a[k]});
                     j++;
                     k--;
+                    while(a[j]==a[j-1])
+                        {
+                        j++;
+                        if(j>=k)
+                         break;
+                        
+                        }
                 }
                 else if(sum>0)
                     k--;
-                else
+                else{
                     j++;
+                    while(a[j]==a[j-1])
+                        {
+                        j++;
+                        if(j>=k)
+                         break;
+                        
+                        }
+                }
+              
             }
+            i++;
+           while(a[i]==a[i-1]){
+              i++;
+               if(i>=n-2)
+                  break;
+            
+           }
         }
-        for(auto x:s)
-            res.push_back(x);
+       
        
         return res;
     }
