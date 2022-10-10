ad8bac3bb1133f7dfef116d0cead9ea89ec5bd98
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-10-10 18:31:07 +0530

    Time: 6 ms (16.14%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
-    bool ispalindrome(string p){
-        string s=p;
-        reverse(s.begin(),s.end());
-        return (s==p);
-    }
     string breakPalindrome(string p) {
-        set<string> s;
-      for(int i=0;i<p.length();i++){
-          int x=p[i];
-          for(char y='a';y<='z';y++){
-              p[i]=y;
-          if(ispalindrome(p))
-              p[i]=x;
-          else
-          {
-              s.insert(p);
-              p[i]=x;
-          }
-          }
-      }
-        
-       for(auto x:s)
-           return x;
-        p="";
+       int n=p.length();
+        if(n==1)
+            return "";
+        for(int i=0;i<n/2;i++)
+        {
+            if(p[i]!='a'){
+                p[i]='a';
+                return p;
+            }
+            
+        }
+        p[n-1]='b';
         return p;
     }
 };
