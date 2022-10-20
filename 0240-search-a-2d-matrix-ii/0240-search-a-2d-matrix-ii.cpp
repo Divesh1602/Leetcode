e4e2c47dee3dbd4329d15cbfe2eabcb60d7e1961
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-10-20 23:37:53 +0530

    Time: 253 ms (36.81%), Space: 14.9 MB (69.04%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,6 +4,8 @@ public:
         int n=a.size();
         int m=a[0].size();
         int r=m-1,t=0;
+         if(a[0][0]>x || a[n-1][m-1]<x)
+           return false;
         while(t<n && r>=0){
             if(a[t][r]==x)
                 return true;
