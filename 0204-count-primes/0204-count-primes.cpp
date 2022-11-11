c2afc5c3bf8510630930eb189e01259260202ee3
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-11-11 20:59:45 +0530

    Time: 245 ms (68.18%), Space: 10.2 MB (68.56%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
    
     int countPrimes(int n) {
-        vector<bool> ans(n,true);
+        vector<bool> ans(n+1,true);
         int ct=0;
-        for(int i=2;i<n;i++){
+        ans[0]=false;
+        ans[1]=false;
+        for(int i=2;i*i<=n;i++){
             if(ans[i]){
-                for(long long int j=(long long)i*i;j<n;j=j+i)
+                for(int j=i*i;j<=n;j=j+i)
                     ans[j]=false;
-               ct++;
+              
             }
+        }
+        for(int i=2;i<n;i++){
+            if(ans[i])
+                ct++;
         }
         
         return ct;
