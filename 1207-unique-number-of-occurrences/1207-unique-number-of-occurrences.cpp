9d8fda4d2f96ed2d57068c2a8cc2d582ca10fa3a
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-11-14 19:18:02 +0530

    Time: 4 ms (69.94%), Space: 8.3 MB (5.78%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int,int> m;
+        for(auto a:arr)
+            m[a]++;
+    
+    multimap<int,int> m1;
+    for(auto a:m){
+        m1.insert({a.second,a.first});
+    }
+       int x=m1.begin()->first;
+       int c=0;
+    for(auto a:m1){
+        if(a.first==x && c==1)
+            return false;
+        x=a.first;
+        c=1;
+    }
+     return true;   
+    }
+    
+};
