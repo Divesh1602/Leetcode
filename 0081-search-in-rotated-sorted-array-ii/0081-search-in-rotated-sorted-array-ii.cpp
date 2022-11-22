4221c364510d83123cc092d03238e2a30986b7a5
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-11-22 12:51:41 +0530

    Time: 11 ms (69.72%), Space: 14 MB (73.83%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     bool search(vector<int>& a, int x) {
-         int l=0,h=a.size()-1;
-        sort(a.begin(),a.end());
+    int l=0,h=a.size()-1;
         while(l<=h){
 		int mid=(l+h)/2;
 		if(a[mid]==x)
 		  return true;
-		if(a[mid]>x)
-            h=mid-1;
-         else
-                l=mid+1;
+            if(a[mid]==a[l] && a[mid]==a[h]){
+                l++;
+                h--;
+            }
+		else if(a[mid]>=a[l]){
+			if(x>=a[l] && x<a[mid]){
+				h=mid-1;
+			}
+			else
+			l=mid+1;
+		}
+		else{
+			if(x>a[mid] && x<=a[h])
+				l=mid+1;
+				else
+				h=mid-1;
+			
+		}
 		
 }
 	return false;
