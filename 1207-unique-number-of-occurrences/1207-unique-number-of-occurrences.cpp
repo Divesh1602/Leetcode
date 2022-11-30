d090a19bf8f473e441e24693ca7be3c27f4761cf
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-11-30 12:30:11 +0530

    Time: 6 ms (54.67%), Space: 8.2 MB (20.75%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -5,19 +5,27 @@ public:
         for(auto a:arr)
             m[a]++;
     
-    multimap<int,int> m1;
-    for(auto a:m){
-        m1.insert({a.second,a.first});
+    // multimap<int,int> m1;
+    // for(auto a:m){
+    //     m1.insert({a.second,a.first});
+    // }
+    //    int x=m1.begin()->first;
+    //    int c=0;
+    // for(auto a:m1){
+    //     if(a.first==x && c==1)
+    //         return false;
+    //     x=a.first;
+    //     c=1;
+    // }
+    //  return true;   
+    // }
+        unordered_set<int> s;
+        for(auto a:m){
+            if(s.find(a.second)==s.end())
+                s.insert(a.second);
+                else 
+                    return false;
+        }
+           return true;  
     }
-       int x=m1.begin()->first;
-       int c=0;
-    for(auto a:m1){
-        if(a.first==x && c==1)
-            return false;
-        x=a.first;
-        c=1;
-    }
-     return true;   
-    }
-    
 };
