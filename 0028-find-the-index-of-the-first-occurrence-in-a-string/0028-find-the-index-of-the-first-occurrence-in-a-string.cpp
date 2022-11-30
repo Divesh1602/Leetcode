d6552fe1b57503612391c46ca5256f3518c611f7
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-11-30 19:52:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (63.52%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,13 +5,17 @@ public:
     int strStr(string txt, string pat) {
        int M=pat.length();
         int N=txt.length();
+        
     int h=1;
+        
     for(int i=1;i<=M-1;i++)
         h=(h*d)%q;
     
     int p=0,t=0;
     for(int i=0;i<M;i++){
+        
         p=(p*d+pat[i])%q;
+        
         t=(t*d+txt[i])%q;
     }
     
@@ -20,14 +24,20 @@ public:
        if(p==t){
            bool flag=true;
            for(int j=0;j<M;j++)
-                if(txt[i+j]!=pat[j]){flag=false;break;}
+                if(txt[i+j]!=pat[j])
+                {
+                    flag=false;
+                    break;
+                }
             if(flag==true)
                 return i;
        }
      
        if(i<N-M){
+           
            t=((d*(t-txt[i]*h))+txt[i+M])%q;
-        if(t<0)t=t+q;
+        
+           if(t<0)t=t+q;
        }
     }
         return -1;
