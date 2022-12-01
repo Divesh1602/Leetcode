89f7629b441c4dbd31ff17c9aa2986f27127acb8
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-12-01 20:00:00 +0530

    Time: 6 ms (19.40%), Space: 6.3 MB (63.63%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,45 +1,50 @@
-#define d 256
-const int q=101;
 class Solution {
 public:
-    int strStr(string txt, string pat) {
-       int M=pat.length();
-        int N=txt.length();
-        
-    int h=1;
-        
-    for(int i=1;i<=M-1;i++)
-        h=(h*d)%q;
-    
-    int p=0,t=0;
-    for(int i=0;i<M;i++){
-        
-        p=(p*d+pat[i])%q;
-        
-        t=(t*d+txt[i])%q;
-    }
-    
-    for(int i=0;i<=(N-M);i++){
-     
-       if(p==t){
-           bool flag=true;
-           for(int j=0;j<M;j++)
-                if(txt[i+j]!=pat[j])
-                {
-                    flag=false;
-                    break;
+      vector<int> prefix(string s,int n){
+        vector<int> lps(n);
+        lps[0]=0;
+        int len=0;
+        int i=1;
+        while(i<n){
+            if(s[i]==s[len]){
+                len++;
+                lps[i]=len;
+                 i++;
+                
+            }
+            else{
+                if(len==0){
+                    lps[i]=0;
+                        i++;
+                    }
+                else{
+                    len=lps[len-1];
                 }
-            if(flag==true)
-                return i;
-       }
-     
-       if(i<N-M){
-           
-           t=((d*(t-txt[i]*h))+txt[i+M])%q;
-        
-           if(t<0)t=t+q;
-       }
+                
+            }
+        }
+        return lps;
     }
+    int strStr(string haystack, string needle) {
+        int m=needle.length();
+        int n=haystack.length();
+    vector<int> ans=prefix(needle,m);
+        int i=0,j=0;
+        while(i<n){
+            if(haystack[i]==needle[j]){
+                i++;
+                j++;
+            }
+            if(j==m)
+                return i-j;
+            else if(i<n && haystack[i]!=needle[j]){
+                if(j==0)
+                    i++;
+                else
+                    j=ans[j-1];
+            }
+            
+        }
         return -1;
     }
 };
