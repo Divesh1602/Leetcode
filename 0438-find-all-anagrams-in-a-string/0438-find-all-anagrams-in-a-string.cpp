b670c7ce09b162096e8233239066d9f9afbaf9bd
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-12-13 20:24:49 +0530

    Time: 124 ms (14.37%), Space: 12 MB (17.25%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    bool issame(int cs[],int cp[]){
-        for(int i=0;i<256;i++)
-            if(cs[i]!=cp[i])
-                return false;
-        return true;
-    }
+    // bool issame(int cs[],int cp[]){
+    //     for(int i=0;i<256;i++)
+    //         if(cs[i]!=cp[i])
+    //             return false;
+    //     return true;
+    // }
     vector<int> findAnagrams(string s, string p) {
         vector<int> ans;
         if(s.length()<p.length())
             return ans;
-        int cs[256]={0},cp[256]={0};
+       unordered_map<char,int> cs,cp;
         for(int i=0;i<p.length();i++){
             cs[s[i]]++;
             cp[p[i]]++;
            
         }
         for(int i=p.length();i<s.length();i++){
-            if(issame(cs,cp)){
+            if(cs==cp){
                 ans.push_back(i-p.length());
             }
             cs[s[i]]++;
+           
             cs[s[i-p.length()]]--;
+             if(cs[s[i-p.length()]]==0)
+                 cs.erase(s[i-p.length()]);
             
         }
-          if(issame(cs,cp)){
+          if(cs==cp){
                 ans.push_back(s.length()-p.length());
             }
         return ans;
