91b44afb7f2489fcd81cde35c9a4327ab917844c
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2022-12-14 14:41:33 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (78.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,12 +4,13 @@ public:
         int n=nums.size();
         if(n==1)
             return nums[0];
-        int a=nums[0],b=max(nums[0],nums[1]);
+    int con=nums[0];
+        int ans=max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-            int tmp=b;
-            b=max(nums[i]+a,b);
-            a=tmp;
+            int temp=ans;
+            ans=max(nums[i]+con,ans);
+            con=temp;
         }
-        return b;
+        return ans;
     }
 };
