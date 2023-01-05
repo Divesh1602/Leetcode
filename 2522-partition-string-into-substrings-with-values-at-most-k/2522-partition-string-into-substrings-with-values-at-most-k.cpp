ece330a4dc4392953d044c1b8b1336712b7d46fe
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-01-05 19:47:01 +0530

    Time: 22 ms (41.91%), Space: 8.9 MB (79.27%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int minimumPartition(string s, int k) {
         int ct=1;
-        string ans="";
+        long long num=0;
         for(int i=0;i<s.length();i++){
-            char str=s[i];
-            long long ch=int(str-'0');
-            if(ch>k)
-                return -1;
-            ans+=s[i];
-            stringstream ss;
-            ss<<ans;
-            long long chk;
-            ss>>chk;
-            if(chk>k){
+        num=num*10+s[i]-'0';
+            if(num>k)
+            {
                 ct++;
-                ans=s[i];
+                num=s[i]-'0';
             }
+            if(num>k)
+                return -1;
         }
         return ct;
     }
