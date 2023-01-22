8be474aaf0af320a4c5a7b2a60cf4dd3d745102a
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-01-22 18:29:34 +0530

    Time: 49 ms (57.21%), Space: 10.8 MB (16.11%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    const int CHAR=256;
     int firstUniqChar(string s) {
-       unordered_map<char,int> store;
-        for(int i=0;i<s.length();i++)
-            store[s[i]]++;
-        for(int i=0;i<s.length();i++){
-            if(store[s[i]]==1)
-                return i;
-        }
+        unordered_map<char,int> m;
+        for(char ch:s)
+            m[ch]++;
+       for(int i=0;i<s.length();i++){
+           if(m[s[i]]==1)
+               return i; 
+       }
         return -1;
     }
 };
