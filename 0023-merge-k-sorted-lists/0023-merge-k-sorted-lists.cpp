904d99339f71dd2ef08dc62a701307ecc1a3d108
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-01-23 23:56:43 +0530

    Time: 175 ms (25.46%), Space: 13.3 MB (58.25%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,56 +10,40 @@
  */
 class Solution {
 public:
-    ListNode* mergeLists(ListNode* list1,ListNode* list2){
-        if(list1==NULL)
-            return list2;
-        if(list2==NULL)
-            return list1;
-        
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)
+            return NULL;
+        if(lists.size()==1)
+            return lists[0];
+        ListNode* list1=lists[0];
+        int i=1;
+       while(i>0){
+           ListNode* list2=lists[i];
           ListNode* head=new ListNode(0);
         ListNode* curr=head;
-            
-            
         while(list1!=NULL && list2!=NULL){
-            
-            
             if(list1->val > list2->val){
                 curr->next=list2;
                 list2=list2->next;
                 curr=curr->next;
             }
-            
             else{
                 curr->next=list1;
                 list1=list1->next;
                 curr=curr->next;
             }
-            
-            
         }
-        
-        
        if(list1!=NULL)
            curr->next=list1;
            
         if(list2!=NULL)
            curr->next=list2;
            
-          return head->next;
-          
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if(lists.size()==0)
-            return NULL;
-        if(lists.size()==1)
-            return lists[0];
-        
-        ListNode* head=mergeLists(lists[0],lists[1]);  
-    
-       for(int i=2;i<lists.size();i++){
-           head=mergeLists(lists[i],head);
-          
+           list1=head->next;
+           if(i==lists.size()-1)
+               return list1;
+           i++;
        }
-        return head;
+        return NULL;
     }
 };
