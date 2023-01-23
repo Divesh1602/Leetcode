4b94127ff1dee868e243f3b5b6464f8e773c7b9d
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-01-23 23:57:41 +0530

    Time: 155 ms (27.33%), Space: 13.3 MB (74.29%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -13,26 +13,45 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.size()==0)
             return NULL;
+        
+        
         if(lists.size()==1)
             return lists[0];
+        
+        
         ListNode* list1=lists[0];
+        
         int i=1;
+        
+        
        while(i>0){
+           
            ListNode* list2=lists[i];
+           
           ListNode* head=new ListNode(0);
+           
         ListNode* curr=head;
+           
+           
         while(list1!=NULL && list2!=NULL){
+            
+            
             if(list1->val > list2->val){
                 curr->next=list2;
                 list2=list2->next;
                 curr=curr->next;
             }
+            
+            
             else{
                 curr->next=list1;
                 list1=list1->next;
                 curr=curr->next;
             }
+            
+            
         }
+           
        if(list1!=NULL)
            curr->next=list1;
            
@@ -44,6 +63,8 @@ public:
                return list1;
            i++;
        }
+        
+        
         return NULL;
     }
 };
