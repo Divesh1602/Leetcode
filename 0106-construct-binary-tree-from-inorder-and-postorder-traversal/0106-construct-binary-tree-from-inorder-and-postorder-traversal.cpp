59e3b05272d3523f7523b32c82e1b32398b7b9f0
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-02-07 19:21:42 +0530

    Time: 8 ms (98.52%), Space: 26.3 MB (62.24%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -12,25 +12,24 @@
 class Solution {
 public:
        int postIndex=0;
-    TreeNode* BuildingTree(vector<int>& post, vector<int>& in,int st,int en){
+    TreeNode* BuildingTree(vector<int>& post,  unordered_map<int,int> &m,int st,int en){
         if(st>en)
             return NULL;
         TreeNode* root=new TreeNode(post[postIndex++]);
         
-        int index;
-        for(int i=st;i<=en;i++){
-            if(in[i]==root->val){
-                index=i;
-                break;
-            }
-        }
+         int val=root->val;
+        int index=m[val];
         
-        root->right=BuildingTree(post,in,index+1,en);
-        root->left=BuildingTree(post,in,st,index-1);
+        root->right=BuildingTree(post,m,index+1,en);
+        root->left=BuildingTree(post,m,st,index-1);
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+         unordered_map<int,int> m;
+        for(int i=0;i<inorder.size();i++){
+            m[inorder[i]]=i;
+        }
         reverse(postorder.begin(),postorder.end());
-         return BuildingTree(postorder,inorder,0,postorder.size()-1);
+         return BuildingTree(postorder,m,0,postorder.size()-1);
     }
 };
