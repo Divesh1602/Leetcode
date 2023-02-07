6f97b24d23b8a57ff181c3afe6ab3573b5c19eef
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-02-07 11:41:49 +0530

    Time: 191 ms (60.15%), Space: 75.3 MB (13.09%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,20 +6,17 @@ public:
         int ans=INT_MIN;
         for(int j=0;j<fruits.size();j++){
             m[fruits[j]]++;
-
             if(m.size()>2){
                 while(m.size()>2){
                     m[fruits[i]]--;
                     if(m[fruits[i]]==0)
                         m.erase(fruits[i]);
-                    
                     i++;
                 }
             }
             ans=max(ans,j-i+1);
         }
-        // for(auto a:m)
-        //     cout<<a.first<<" "<<a.second<<endl;
+       
         return ans;
     }
 };
