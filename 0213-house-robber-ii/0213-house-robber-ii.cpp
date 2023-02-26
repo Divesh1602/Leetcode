7d79c17f9cee75e82225bca2a3d07b3f9c07ae50
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-02-26 23:27:18 +0530

    Time: 5 ms (25.97%), Space: 7.8 MB (67.81%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -4,24 +4,29 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size()==1)
             return nums[0];
-         vector<int> dp(nums.size(),0),dp1(nums.size(),0);
-        dp[0]=nums[0];
-        dp1[1]=nums[1];
+         
+        int prev1=0,prev=nums[0];
+     
         for(int i=1;i<nums.size()-1;i++){
             int take=nums[i];
             if(i>1)
-                take+=dp[i-2];
-            int notTake=dp[i-1];
-            dp[i]=max(take,notTake);          
+                take+=prev1;
+            int notTake=prev;
+           int curr=max(take,notTake);   
+            prev1=prev;
+            prev=curr;
         }
+        int prev3=0,prev2=nums[1];
          for(int i=2;i<nums.size();i++){
             int take=nums[i];
              if(i>1)
-                take+=dp1[i-2];
-            int notTake=dp1[i-1];
-            dp1[i]=max(take,notTake);          
+                take+=prev3;
+            int notTake=prev2;
+            int curr=max(take,notTake);   
+            prev3=prev2;
+            prev2=curr;        
         }
-        int n=dp.size(),m=dp1.size();
-        return max(dp[n-2],dp1[m-1]);
+     
+        return max(prev,prev2);
     }
 };
