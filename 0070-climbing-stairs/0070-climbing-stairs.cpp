118de42fbbba0ce8998c75ca565902ed5195e157
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-02-26 19:24:57 +0530

    Time: 3 ms (40.02%), Space: 6.3 MB (19.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n <= 2) return n;
-        int prev = 2, prev2 = 1, res;
-        for (int i = 3; i <= n; i++) {
-            res = prev + prev2;
-            prev2 = prev;
-            prev = res;
+    int helper(int n,int& ans,vector<int>& dp){
+        if(n<0){
+          
+          return 0;
+      }
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        if(n==0){
+            dp[n]=1;
+            return 1;
         }
-        return res;
+     
+         
+        
+        
+        dp[n]=helper(n-1,ans,dp)+helper(n-2,ans,dp);
+        
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        int ans=0;
+        return helper(n,ans,dp);
+        
     }
 };
