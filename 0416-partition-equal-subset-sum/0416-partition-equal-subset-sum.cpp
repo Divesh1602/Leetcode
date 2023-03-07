5fcc26ac3067a7f88130b384f1603ff98ae5c40e
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-03-07 19:57:58 +0530

    Time: 521 ms (18.93%), Space: 13.2 MB (62.20%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -4,6 +4,7 @@ public:
         vector<vector<bool>> dp(n,vector<bool>(k+1,false));
         for(int i=0;i<n;i++)
             dp[i][0]=true;
+        if(nums[0]<=k)
         dp[0][nums[0]]=true;
         
         for(int i=1;i<n;i++){
@@ -29,10 +30,10 @@ public:
         
         if(sum%2==1)
             return false;
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
         int n=nums.size();
-        if(n==1)
-            return false;
+        // if(n==1)
+        //     return false;
           return helper(nums,n,sum/2);
     }
 };
