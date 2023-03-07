5d4ee4bb5b7ba5d25c6cad3bccd876d5e8df0d5a
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-03-07 19:49:37 +0530

    Time: 446 ms (29.62%), Space: 13.3 MB (62.20%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool helper(vector<int>& nums,int n,int k){
+        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+        for(int i=0;i<n;i++)
+            dp[i][0]=true;
+        dp[0][nums[0]]=true;
+        
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=k;j++){
+                 bool notTake=dp[i-1][j];
+                 bool take=false;
+                 if(nums[i]<=j)
+                   take=dp[i-1][j-nums[i]];
+        
+                 dp[i][j]= take | notTake;
+            }
+        }
+        
+        
+        return dp[n-1][k];    
+    }
+    
+    bool canPartition(vector<int>& nums) {
+     
+        int sum=0;
+        for(auto a:nums)
+            sum+=a;
+        
+        if(sum%2==1)
+            return false;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n==1)
+            return false;
+          return helper(nums,n,sum/2);
+    }
+};
