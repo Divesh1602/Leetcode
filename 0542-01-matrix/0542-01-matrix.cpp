6d4ad3213c2aae80a88ccb9c1e9b269b02d3305d
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-03-14 18:11:52 +0530

    Time: 99 ms (38.77%), Space: 34.5 MB (15.71%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int rows = mat.size();
-        int cols = mat[0].size();
-        vector<vector<int>> vis(rows,vector<int>(cols,0));
-        vector<vector<int>> result(rows,vector<int>(cols,0));;
-        queue<pair<pair<int,int>,int>> q;
-        for(int i =0; i<rows;i++)
-        {
-            for(int j = 0;j<cols;j++){
-                if(mat[i][j] == 0){
-                    q.push({{i,j},0});
-                    vis[i][j]=1;
-                }
-            }
-        }
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
-        while(!q.empty()){
-            int sr = q.front().first.first;
-            int sc = q.front().first.second;
-            int dis = q.front().second;
-            result[sr][sc] = dis;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int delR = sr + delRow[i];
-                int delC = sc + delCol[i];
-                if(delR>=0 && delR<rows && delC>=0 && delC<cols &&vis[delR][delC] == 0 && mat[delR][delC] == 1){
-                    q.push({{delR,delC},dis+1});
-                    vis[delR][delC]=1;
-
-                }
-            }
-
-            
-
-        }
-        return result;
-
-        
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+         int n=grid.size();
+	    int m=grid[0].size();
+	    vector<vector<int>> vis(n,vector<int>(m,0));
+	    vector<vector<int>> dis(n,vector<int>(m,0));
+	    queue<pair<pair<int,int>,int>> q;
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(grid[i][j]==0){
+	                vis[i][j]=1;
+	                q.push({{i,j},0});
+	            }
+	        }
+	    }
+	    int delrow[]={-1,0,1,0};
+	    int delcol[]={0,1,0,-1};
+	    while(!q.empty()){
+	        int row=q.front().first.first;
+	        int col=q.front().first.second;
+	        int steps=q.front().second;
+	        q.pop();
+	        dis[row][col]=steps;
+	        for(int i=0;i<4;i++){
+	            int nrow=row+delrow[i];
+	            int ncol=col+delcol[i];
+	            
+	            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol]){
+	                vis[nrow][ncol]=1;
+	                q.push({{nrow,ncol},steps+1});
+	            }
+	        }
+	    }
+	    return dis;
     }
 };
