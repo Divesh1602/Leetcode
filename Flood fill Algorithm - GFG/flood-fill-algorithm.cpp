b51419b0f26728ba5815aa012ff1c3a26048a4b3
Divesh1602 <110462335+Divesh1602@users.noreply.github.com>
2023-03-14 13:42:03 +0530

    Added solution - LeetHub

---
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // Code here 
+        int n=image.size(),m=image[0].size();
+        int ele=image[sr][sc];
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        image[sr][sc]=color;
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vis[sr][sc]=1;
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            if(row+1<n && image[row+1][col]==ele && !vis[row+1][col]){
+                image[row+1][col]=color;
+                vis[row+1][col]=1;
+                q.push({row+1,col});
+            }
+            if(row-1>=0 && image[row-1][col]==ele && !vis[row-1][col]){
+                image[row-1][col]=color;
+                vis[row-1][col]=1;
+                q.push({row-1,col});
+            }
+            if(col-1>=0 && image[row][col-1]==ele && !vis[row][col-1]){
+                image[row][col-1]=color;
+                 vis[row][col-1]=1;
+                q.push({row,col-1});
+            }
+            if(col+1<m && image[row][col+1]==ele && !vis[row][col+1]){
+                image[row][col+1]=color;
+                vis[row][col+1]=1;
+                q.push({row,col+1});
+            }
+        }
+        return image;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>image(n, vector<int>(m,0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++)
+				cin >> image[i][j];
+		}
+		int sr, sc, newColor;
+		cin >> sr >> sc >> newColor;
+		Solution obj;
+		vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+		for(auto i: ans){
+			for(auto j: i)
+				cout << j << " ";
+			cout << "\n";
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
